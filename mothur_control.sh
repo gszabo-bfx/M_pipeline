99ab4390e1308816f06c81bc870da64d56842392
Gyula Szabo <gyula.szabo.bfx@gmail.com>
2025-11-16 15:40:23 +0100

    cutadapt in mothur_control.sh works with inline parameters -p path -a assay -n R1_string

--- mothur_control.sh
+++ mothur_control.sh
@@ -3,42 +3,56 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -p path-to-project"
+   echo "Usage: $0 -p path -a assay -n R1_name_pattern"
    echo -e "\t-p path : path/to/project/folder"
-   # echo -e "\t-b Description of what is parameterB"
-   # echo -e "\t-c Description of what is parameterC"
+   echo -e "\t-a assay : amplicon target assay eg [ILMNV3V4] or [PATE]"
+   echo -e "\t-n R1_name_pattern : pattern to recognise R1 read eg [_R1_001.fastq.gz] or [_1.fq.gz]"
    exit 1 # Exit script after printing help
 }
 
-while getopts "p:b:c:" opt
+while getopts "p:a:n:" opt
 do
    case "$opt" in
       p ) path="$OPTARG" ;;
-      # b ) parameterB="$OPTARG" ;;
-      # c ) parameterC="$OPTARG" ;;
+      a ) assay="$OPTARG" ;;
+      n ) parameterC="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$path" ] # || [ -z "$parameterB" ] || [ -z "$parameterC" ]
+if [ -z "$path" ] || [ -z "$assay" ] #|| [ -z "$parameterC" ]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
 fi
 
+
+
+################
 # Begin script in case all parameters are correct
 #echo "$parameterA"
 #echo "$parameterB"
 #echo "$parameterC"
 
 
+#################
+# prep project folder
+if [ ! -d ${path}/fq_in ]
+then
+	mkdir ${path}/fq_in
+fi
 
+if [ ! -d ${path}/cut_out ]
+then
+	mkdir ${path}/cut_out
+fi
 
-# usage script path/to/project/folder 
+if [ ! -d ${path}/log_out ]
+then
+	mkdir ${path}/log_out
+fi
 
-mkdir ${path}/fq_in
-mkdir ${path}/cut_out
 
 # -path "./fq_in" -prune -o -> exclude "./fq_in" and continue others
 find ${path} -path "./fq_in" -prune -o -name "*.fastq.gz" -print -exec cp {} ${path}/fq_in \;
@@ -50,14 +64,46 @@ find ${path} -path "./fq_in" -prune -o -name "*.fastq" -print -exec rm {} ${path
 find ${path} -path "./fq_in" -prune -o -name "*.fq" -print -exec cp {} ${path}/fq_in \;
 find ${path} -path "./fq_in" -prune -o -name "*.fq" -print -exec rm {} ${path}/fq_in \;
 
-for i in ${path}/fq_in/*R1_001.fastq.gz
+#################
+# select assay
+# source https://linuxize.com/post/bash-case-statement/
+case $assay in
+
+  ILMNV3V4)
+    R1_FW="CCTACGGGNGGCWGCAG"
+    R1_RV="GGATTAGATACCCBDGTAGTC"
+    R2_FW="GACTACHVGGGTATCTAATCC"
+    R2_RV="CTGCWGCCNCCCGTAGG"
+    ;;
+
+  PATE)
+    R1_FW="GTGYCAGMAGBNKCGGTVA"
+    R1_RV="ATTAGADACCYBNKTAGTCY"
+    R2_FW="RGACTAMNVRGGTHTCTAAT"
+    R2_RV="TBACCGMNVCTKCTGRCAC"
+    ;;
+
+  #PATTERN_N)
+  #  STATEMENTS
+  #  ;;
+
+  *)
+	  echo -e "\n\nAssay name is not valid"
+    helpFunction
+    ;;
+esac
+
+
+#################
+# cutadapt
+for i in ${path}/fq_in/*_R1_001.fastq.gz
 do
 	R1_base=$(basename "$i")
 	R2_base="$(sed "s/R1/R2/g" <<< $R1_base)"
 
 cutadapt \
-	-g CCTACGGGNGGCWGCAG -a GGATTAGATACCCBDGTAGTC \
-	-G GACTACHVGGGTATCTAATCC -A CTGCWGCCNCCCGTAGG \
+	-g $R1_FW -a $R1_RV \
+	-G $R2_FW -A $R2_RV \
 	--cores=8 \
 	--discard-untrimmed \
 	--max-n 0 \
@@ -65,7 +111,8 @@ cutadapt \
 	-o ${path}/cut_out/$R1_base \
 	-p ${path}/cut_out/$R2_base \
 	${path}/fq_in/$R1_base \
-	${path}/fq_in/$R2_base
+	${path}/fq_in/$R2_base \
+	 | tee ${path}/log_out/cutadapt_stdout.txt
 done
 
 gzip ${path}/fq_in/*.fastq
